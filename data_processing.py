5da1305f110df0061c58a1b4b4b89a67de047ee8
Eric Liu <ericliu404@gmail.com>
2019-11-02 17:17:10 -0400

    Add csv parsing

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import numpy as np
 
 
 class DataProcessing:
     def __init__(self, train_images_filename, test_images_filename):
         self.train_images = pd.read_pickle(train_images_filename)
         self.test_images = pd.read_pickle(test_images_filename)
+        self.train_y = None
+
+    def parse_csv(self, filename):
+        with open(filename, newline='') as file:
+            dataframe = pd.read_csv(file)
+            self.train_y = np.array(dataframe)
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from data_processing import DataProcessing
 
 def main():
     dp = DataProcessing('train_max_x', 'test_max_x')
+    dp.parse_csv('train_max_y.csv')
     print(dp.train_images)
     print(dp.test_images)
+    print(dp.train_y)
 
 
 main()
