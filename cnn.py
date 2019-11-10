0506d6730c1a14ed5bad1005e7a28f88bc61b3be
Eric Liu <ericliu404@gmail.com>
2019-11-10 14:31:26 -0500

    Add alternative cnn for basic mnist

--- cnn.py
+++ cnn.py
@@ -8,12 +8,12 @@ from torch.autograd import Variable
 from sklearn.model_selection import train_test_split
 
 # constants
-EPOCHS = 5
+EPOCHS = 4
 BATCH_SIZE = 32
 
 # input and target data
-X = pd.read_pickle('train_max_x')[:500]
-y = np.array(pd.read_csv('train_max_y.csv'))[:500, 1]
+X = pd.read_pickle('train_max_x')[:1000]
+y = np.array(pd.read_csv('train_max_y.csv'))[:1000, 1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
 
@@ -90,26 +90,28 @@ test_loader = torch.utils.data.DataLoader(test, batch_size=BATCH_SIZE, shuffle=F
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=5)
-        # self.fc1 = nn.Linear(3 * 3 * 64, 256)
-        self.fc1 = nn.Linear(64 * 28 * 28, 256)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.fc1 = nn.Linear(64 * 64 * 8, 256)
         self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        # x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(F.max_pool2d(self.conv3(x), 2))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = x.view(-1, 64 * 28 * 28)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
 
 
 cnn = CNN()
